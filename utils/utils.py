bbaf012e2f47d93950235aa72c8703544e4e76f2
Stephen-SMJ <s2371160103@163.com>
2025-07-04 17:46:44 +0800

    integrate utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,22 @@
 import re
 
+def extract_code(text: str) -> tuple[bool, Any]:
+    pattern = r'```python([^\n]*)(.*?)```'
+    matches = re.findall(pattern, text, re.DOTALL)
+    if len(matches)>1:
+        code_blocks = ''
+        for match in matches:
+            code_block = match[1]
+            code_blocks += code_block
+        return True, code_blocks
+    elif len(matches):
+        code_block = matches[-1]
+        #if 'python' in code_block[0]:
+        return True, code_block[1]
+    else:
+        return False, ''
+
+
 def extract_suggestion(text):
     '''
         Next, you can:
